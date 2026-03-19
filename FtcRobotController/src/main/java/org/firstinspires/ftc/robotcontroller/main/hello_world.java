8d86085b5fcd287d8278bf36624021911a9d7102
CT-9415 <nicholas.pechotta28@d155.org>
2026-03-19 12:17:28 -0500

    Updated movement script, untested

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/main/hello_world.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/main/hello_world.java
@@ -1,70 +1,76 @@
+
 package org.firstinspires.ftc.robotcontroller.main;
 
-//import op mode
-import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
-//import motors
-import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
-public class hello_world extends OpMode {
+@TeleOp
+public class hello_world extends LinearOpMode {
 
-    private DcMotorEx leftFront, leftBack, rightFront, rightBack;
-    private IMU imu;
-
-    public void init(HardwareMap hardwareMap){
-        leftFront=hardwareMap.get(DcMotorEx.class, "lF");
-        leftBack=hardwareMap.get(DcMotorEx.class, "lB");
-        rightFront=hardwareMap.get(DcMotorEx.class, "rF");
-        rightBack=hardwareMap.get(DcMotorEx.class, "rB");
-
-        leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
-        leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
-
-        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-        //----------------IMU--------------
-        imu= hardwareMap.get(IMU.class,"imu");
-
-        RevHubOrientationOnRobot revHubOrientation = new RevHubOrientationOnRobot(
-                RevHubOrientationOnRobot.LogoFacingDirection.UP,
-                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD
-        );
-        imu.initialize(new IMU.Parameters(revHubOrientation));
-
-    }
-    public void drive (double forward, double strafe, double rotate){
-        double leftFrontPower = forward + strafe + rotate;
-        double leftBackPower = forward - strafe + rotate;
-        double rightFrontPower = forward - strafe - rotate;
-        double rightBackPower = forward + strafe - rotate;
-
-        double maxPower = 1;
-        double maxSpeed = 1;
-
-        maxPower = Math.max(maxPower, Math.abs(leftFrontPower));
-        maxPower = Math.max(maxPower, Math.abs(leftBackPower));
-        maxPower = Math.max(maxPower, Math.abs(rightFrontPower));
-        maxPower = Math.max(maxPower, Math.abs(rightBackPower));
-
-        leftFront.setPower(maxSpeed * (leftFrontPower / maxPower));
-        leftBack.setPower(maxSpeed * (leftBackPower / maxPower));
-        rightFront.setPower(maxSpeed * (rightFrontPower / maxPower));
-        rightBack.setPower(maxSpeed * (rightBackPower / maxPower));
-
-    }
+    // Declare OpMode members for each of the 4 motors.
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor frontLeftDrive = null;
+    private DcMotor backLeftDrive = null;
+    private DcMotor frontRightDrive = null;
+    private DcMotor backRightDrive = null;
 
     @Override
-    void loop() {
+    public void runOpMode() {
 
+        frontLeftDrive = hardwareMap.get(DcMotor.class, "front_left_drive");
+        backLeftDrive = hardwareMap.get(DcMotor.class, "back_left_drive");
+        frontRightDrive = hardwareMap.get(DcMotor.class, "front_right_drive");
+        backRightDrive = hardwareMap.get(DcMotor.class, "back_right_drive");
+
+        frontLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+        backLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+        frontRightDrive.setDirection(DcMotor.Direction.FORWARD);
+        backRightDrive.setDirection(DcMotor.Direction.FORWARD);
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+        runtime.reset();
+
+        while (opModeIsActive()) {
+            double max;
+
+            double axial = 0.3;
+            double lateral =  0;
+            double yaw  =  0;
+
+            double frontLeftPower  = axial + lateral + yaw;
+            double frontRightPower = axial - lateral - yaw;
+            double backLeftPower   = axial - lateral + yaw;
+            double backRightPower  = axial + lateral - yaw;
+
+            max = Math.max(Math.abs(frontLeftPower), Math.max(Math.abs(frontRightPower), Math.max(Math.abs(backLeftPower), Math.abs(backRightPower))));
+
+            backLeftPower = ClampWheels(backLeftPower, max);
+            frontLeftPower = ClampWheels(frontLeftPower, max);
+            backRightPower = ClampWheels(backRightPower, max);
+            frontRightPower = ClampWheels(frontRightPower, max);
+
+            frontLeftDrive.setPower(frontLeftPower);
+            frontRightDrive.setPower(frontRightPower);
+            backLeftDrive.setPower(backLeftPower);
+            backRightDrive.setPower(backRightPower);
+
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("Front left/Right", "%4.2f, %4.2f", frontLeftPower, frontRightPower);
+            telemetry.addData("Back  left/Right", "%4.2f, %4.2f", backLeftPower, backRightPower);
+            telemetry.update();
+        }
+    }
+    public double ClampWheels(double power, double max) {
+        if (max > 1.0) {
+            power /= max;
+        }
+        return power;
     }
 }
+
