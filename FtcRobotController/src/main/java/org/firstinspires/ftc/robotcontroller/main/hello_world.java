a309d0cadc03090e8b71f2a30ea47c43f7abc349
CT-9415 <nicholas.pechotta28@d155.org>
2026-03-20 12:17:03 -0500

    Updated movement again, still untested

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/main/hello_world.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/main/hello_world.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp
@@ -12,23 +13,23 @@ public class hello_world extends LinearOpMode {
 
     // Declare OpMode members for each of the 4 motors.
     private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor frontLeftDrive = null;
-    private DcMotor backLeftDrive = null;
-    private DcMotor frontRightDrive = null;
-    private DcMotor backRightDrive = null;
+    private DcMotorEx frontLeft = null;
+    private DcMotorEx backLeft = null;
+    private DcMotorEx frontRight = null;
+    private DcMotorEx backRight = null;
 
     @Override
     public void runOpMode() {
 
-        frontLeftDrive = hardwareMap.get(DcMotor.class, "front_left_drive");
-        backLeftDrive = hardwareMap.get(DcMotor.class, "back_left_drive");
-        frontRightDrive = hardwareMap.get(DcMotor.class, "front_right_drive");
-        backRightDrive = hardwareMap.get(DcMotor.class, "back_right_drive");
+        frontLeft = hardwareMap.get(DcMotorEx.class, "lF");
+        backLeft = hardwareMap.get(DcMotorEx.class, "lB");
+        frontRight = hardwareMap.get(DcMotorEx.class, "rF");
+        backRight = hardwareMap.get(DcMotorEx.class, "rB");
 
-        frontLeftDrive.setDirection(DcMotor.Direction.REVERSE);
-        backLeftDrive.setDirection(DcMotor.Direction.REVERSE);
-        frontRightDrive.setDirection(DcMotor.Direction.FORWARD);
-        backRightDrive.setDirection(DcMotor.Direction.FORWARD);
+        frontLeft.setDirection(DcMotor.Direction.REVERSE);
+        backLeft.setDirection(DcMotor.Direction.REVERSE);
+        frontRight.setDirection(DcMotor.Direction.FORWARD);
+        backRight.setDirection(DcMotor.Direction.FORWARD);
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -55,10 +56,10 @@ public class hello_world extends LinearOpMode {
             backRightPower = ClampWheels(backRightPower, max);
             frontRightPower = ClampWheels(frontRightPower, max);
 
-            frontLeftDrive.setPower(frontLeftPower);
-            frontRightDrive.setPower(frontRightPower);
-            backLeftDrive.setPower(backLeftPower);
-            backRightDrive.setPower(backRightPower);
+            frontLeft.setPower(frontLeftPower);
+            frontRight.setPower(frontRightPower);
+            backLeft.setPower(backLeftPower);
+            backRight.setPower(backRightPower);
 
             telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.addData("Front left/Right", "%4.2f, %4.2f", frontLeftPower, frontRightPower);
