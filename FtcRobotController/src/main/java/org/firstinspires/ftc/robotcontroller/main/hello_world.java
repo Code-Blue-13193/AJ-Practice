94419a1880f02ce015939165b7ae97625f7b0037
CT-9415 <nicholas.pechotta28@d155.org>
2026-03-30 12:30:52 -0500

    Can now move forward! (Tested)
    (Untested)-Stop movement, moving horizontally

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/main/hello_world.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/main/hello_world.java
@@ -21,10 +21,10 @@ public class hello_world extends LinearOpMode {
     @Override
     public void runOpMode() {
 
-        frontLeft = hardwareMap.get(DcMotorEx.class, "lF");
-        backLeft = hardwareMap.get(DcMotorEx.class, "lB");
-        frontRight = hardwareMap.get(DcMotorEx.class, "rF");
-        backRight = hardwareMap.get(DcMotorEx.class, "rB");
+        frontLeft = hardwareMap.get(DcMotorEx.class, "lf");
+        backLeft = hardwareMap.get(DcMotorEx.class, "lb");
+        frontRight = hardwareMap.get(DcMotorEx.class, "rf");
+        backRight = hardwareMap.get(DcMotorEx.class, "rb");
 
         frontLeft.setDirection(DcMotor.Direction.REVERSE);
         backLeft.setDirection(DcMotor.Direction.REVERSE);
@@ -38,40 +38,59 @@ public class hello_world extends LinearOpMode {
         runtime.reset();
 
         while (opModeIsActive()) {
-            double max;
-
-            double axial = 0.3;
-            double lateral =  0;
-            double yaw  =  0;
-
-            double frontLeftPower  = axial + lateral + yaw;
-            double frontRightPower = axial - lateral - yaw;
-            double backLeftPower   = axial - lateral + yaw;
-            double backRightPower  = axial + lateral - yaw;
-
-            max = Math.max(Math.abs(frontLeftPower), Math.max(Math.abs(frontRightPower), Math.max(Math.abs(backLeftPower), Math.abs(backRightPower))));
-
-            backLeftPower = ClampWheels(backLeftPower, max);
-            frontLeftPower = ClampWheels(frontLeftPower, max);
-            backRightPower = ClampWheels(backRightPower, max);
-            frontRightPower = ClampWheels(frontRightPower, max);
-
-            frontLeft.setPower(frontLeftPower);
-            frontRight.setPower(frontRightPower);
-            backLeft.setPower(backLeftPower);
-            backRight.setPower(backRightPower);
-
-            telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.addData("Front left/Right", "%4.2f, %4.2f", frontLeftPower, frontRightPower);
-            telemetry.addData("Back  left/Right", "%4.2f, %4.2f", backLeftPower, backRightPower);
-            telemetry.update();
+            RawMove(0.3, 0, 0);
+            if (getRuntime()>=1) {
+                RawMove(0.3, 0.3, 0);
+            }
         }
     }
-    public double ClampWheels(double power, double max) {
+    public double ClampWheel(double power, double max) {
         if (max > 1.0) {
             power /= max;
         }
         return power;
+    }
+
+    public void RawMove(double axial, double lateral, double yaw) {
+        //axial is total movement power
+        //lateral is horizontal movement power (from the robots pov)
+        //yaw rotates the robot
+
+        //misc var declaration
+        double max;
+
+        //calc motor power
+        double frontLeftPower  = axial + lateral + yaw;
+        double frontRightPower = axial - lateral - yaw;
+        double backLeftPower   = axial - lateral + yaw;
+        double backRightPower  = axial + lateral - yaw;
+
+        //limit motor powers
+        max = Math.max(Math.abs(frontLeftPower), Math.max(Math.abs(frontRightPower), Math.max(Math.abs(backLeftPower), Math.abs(backRightPower))));
+
+        backLeftPower = ClampWheel(backLeftPower, max);
+        frontLeftPower = ClampWheel(frontLeftPower, max);
+        backRightPower = ClampWheel(backRightPower, max);
+        frontRightPower = ClampWheel(frontRightPower, max);
+
+        //apply motor powers
+        frontLeft.setPower(frontLeftPower);
+        frontRight.setPower(frontRightPower);
+        backLeft.setPower(backLeftPower);
+        backRight.setPower(backRightPower);
+
+        //debug
+        telemetry.addData("Status", "Run Time: " + runtime.toString());
+        telemetry.addData("Front left/Right", "%4.2f, %4.2f", frontLeftPower, frontRightPower);
+        telemetry.addData("Back  left/Right", "%4.2f, %4.2f", backLeftPower, backRightPower);
+        telemetry.update();
+    }
+
+    public void StopMove() {
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+        backLeft.setPower(0);
+        backRight.setPower(0);
     }
 }
 
